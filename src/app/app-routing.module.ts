87fef97ffd8c1ca8d93b37cf7d63e901e91c7c00
giangrobert <gianmamani93@gmail.com>
2020-06-26 07:00:27 -0500

    Primer avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './web/home/home.component';
+import { ServiciosComponent } from './web/servicios/servicios.component';
+import { ProductosComponent } from './web/productos/productos.component';
+import { RegistroComponent } from './web/registro/registro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'servicio', component:ServiciosComponent},
+  {path: 'producto', component:ProductosComponent},
+  {path:'registro', component:RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './web/home/home.component';
+import { ServiciosComponent } from './web/servicios/servicios.component';
+import { ProductosComponent } from './web/productos/productos.component';
+import { RegistroComponent } from './web/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServiciosComponent,
+    ProductosComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
