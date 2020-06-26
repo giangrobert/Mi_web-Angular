92cdf546932b36a10c3bf4fd44e8165bf37b43fd
giangrobert <gianmamani93@gmail.com>
2020-06-26 15:00:40 -0500

    Miweb ngular completo

--- src/app/web/productos/productos.component.ts
+++ src/app/web/productos/productos.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-productos',
+  selector: 'producto',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
