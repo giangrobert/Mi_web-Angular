0c129d83060f3a24d720984abf31848ccde33966
giangrobert <gianmamani93@gmail.com>
2020-07-03 12:25:13 -0500

    Avance 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB-HX5RJN-4_ccJBAW2pNz4dmPXRkvRfvg",
+    authDomain: "ventabd2.firebaseapp.com",
+    databaseURL: "https://ventabd2.firebaseio.com",
+    projectId: "ventabd2",
+    storageBucket: "ventabd2.appspot.com",
+    messagingSenderId: "371540699873",
+    appId: "1:371540699873:web:1ae086712e4ff37ddba5a0",
+    measurementId: "G-HX7B445L3F"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyB-HX5RJN-4_ccJBAW2pNz4dmPXRkvRfvg",
     authDomain: "ventabd2.firebaseapp.com",
